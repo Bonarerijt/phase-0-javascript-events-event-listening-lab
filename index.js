6df595e75858ee67d69e19378ed7184117fe9d4b
Bonarerijt <prudyjj4@gmail.com>
2025-09-26 22:55:19 +0300

    created event listeners on DOM nodes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// function addingEventListener() {
+// }
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('Button clicked!');
+  });
 }
